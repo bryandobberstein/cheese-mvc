a2669e0be9592c88af330eac704b2225a753bb84
Bryan Maxwell Dobberstein <max.dobberstein@gmail.com>
2017-05-17 17:15:22 -0500

    Studio part 4 complete

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -1,8 +1,10 @@
 package org.launchcode.cheesemvc.controllers;
 
 import org.launchcode.cheesemvc.models.Cheese;
+import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,11 +18,10 @@ import java.util.ArrayList;
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
-    static ArrayList<Cheese> cheeses = new ArrayList<>();
 
     @RequestMapping(value = "")
     public String index(Model model){
-        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("cheeses", CheeseData.getAll());
         model.addAttribute("title", "Get Cheesy");
         return "cheese/index";
     }
@@ -32,9 +33,25 @@ public class CheeseController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAdd(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
-        Cheese newCheese = new Cheese(cheeseName, cheeseDescription);
-        cheeses.add(newCheese);
+    public String processAdd(@ModelAttribute Cheese cheese){
+        CheeseData.add(cheese);
+        return "redirect:";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.GET)
+    public String remove(Model model){
+        model.addAttribute("cheeses", CheeseData.getAll());
+        model.addAttribute("title", "Remove Cheese");
+        return "cheese/remove";
+    }
+
+    @RequestMapping(value = "remove", method = RequestMethod.POST)
+    public String processRemove(@RequestParam int[] cheeseIds) {
+
+        for (int id : cheeseIds){
+            CheeseData.remove(id);
+        }
+
         return "redirect:";
     }
 }
