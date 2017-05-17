33455a556675e45a0352039920b20703dc46f621
Bryan Maxwell Dobberstein <max.dobberstein@gmail.com>
2017-05-17 13:24:54 -0500

    Studio part 2 complete

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,6 +3,10 @@ package org.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
 
 /**
  * Created by max on 5/16/17.
@@ -11,9 +15,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "cheese")
 public class CheeseController {
 
+    static HashMap<String, String> cheeses = new HashMap<>();
+
     @RequestMapping(value = "")
     public String index(Model model){
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "Get Cheesy");
         return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model){
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAdd(@RequestParam String cheeseName, @RequestParam String cheeseDescription){
+        cheeses.put(cheeseName, cheeseDescription);
+        return "redirect:";
     }
 }
